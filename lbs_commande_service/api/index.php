8c4d2a9051e5b1dd378b35606568a4d25b401dc7
GIRARDIN-VINCENT Bastien <bastien52300@hotmail.fr>
2021-01-26 17:22:59 +0100

    same

--- lbs_commande_service/api/index.php
+++ lbs_commande_service/api/index.php
@@ -1,15 +1,15 @@
 <?php
 echo "commande service index";
-#require_once  __DIR__ . '/../src/vendor/autoload.php';
+require_once  __DIR__ . '/../src/vendor/autoload.php';
 
-#use \Psr\Http\Message\ServerRequestInterface as Request ;
-#use \Psr\Http\Message\ResponseInterface as Response ;
+use \Psr\Http\Message\ServerRequestInterface as Request ;
+use \Psr\Http\Message\ResponseInterface as Response ;
 
-// $app = new \Slim\App();
+$app = new \Slim\App();
 
-// $app->get('/test', function (Request $rq, Response $rs): Response {
-//     $rs->getBody()->write('Hello world');
-//     return $rs;
-// });
+$app->get('/test', function (Request $rq, Response $rs): Response {
+    $rs->getBody()->write('Hello world');
+    return $rs;
+});
 
-// $app->run();
+$app->run();
