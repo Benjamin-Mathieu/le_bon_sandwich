3db14ffb35891635cdd7e626169905d435ea4903
Benjamin <ben88200@gmail.com>
2021-01-26 15:31:38 +0100

    Ajout fichier projet

---
+++ lbs_catalogue_service/api/index.php
@@ -0,0 +1,8 @@
+<?php
+
+#require_once  __DIR__ . '/../src/vendor/autoload.php';
+
+#use \Psr\Http\Message\ServerRequestInterface as Request ;
+#use \Psr\Http\Message\ResponseInterface as Response ;
+
+echo "catalogue service index";
---
+++ lbs_commande_service/api/index.php
@@ -0,0 +1,15 @@
+<?php
+echo "commande service index";
+#require_once  __DIR__ . '/../src/vendor/autoload.php';
+
+#use \Psr\Http\Message\ServerRequestInterface as Request ;
+#use \Psr\Http\Message\ResponseInterface as Response ;
+
+// $app = new \Slim\App();
+
+// $app->get('/test', function (Request $rq, Response $rs): Response {
+//     $rs->getBody()->write('Hello world');
+//     return $rs;
+// });
+
+// $app->run();
