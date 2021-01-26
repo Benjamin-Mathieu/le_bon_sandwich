5da5f5c4bab3b1f52e18c15d5145ce4d5a0e5faf
Benjamin <ben88200@gmail.com>
2021-01-26 17:29:44 +0100

    Ajout route /sandwichs

--- lbs_catalogue_service/api/index.php
+++ lbs_catalogue_service/api/index.php
@@ -5,17 +5,22 @@ require_once  __DIR__ . '/../src/vendor/autoload.php';
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-echo "catalogue service index";
+echo "<h2>catalogue service index</h2>";
 
 // Connexion à la DB
 $connection = new \MongoDB\Client("mongodb://dbcat");
-
 // Sélectionne la base de donnée à utiliser
 $db_catalogue = $connection->catalogue;
-
 // Sélectionne la collection de sandwichs
 $sandwiches = $db_catalogue->sandwiches->find();
 
 foreach ($sandwiches as $sandwich) {
     print $sandwich->nom . ' ' . $sandwich->type_pain . '<br>';
 }
+
+$app = new \Slim\App();
+
+$app->get("/sandwichs", function (Request $rq, Response $resp) {
+});
+
+$app->run();
