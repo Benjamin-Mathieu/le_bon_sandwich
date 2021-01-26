17d5dbd9f84d2f609623cb1070a271ab0381fd6a
Benjamin <ben88200@gmail.com>
2021-01-26 18:06:25 +0100

    Fix route

--- lbs_catalogue_service/api/index.php
+++ lbs_catalogue_service/api/index.php
@@ -5,8 +5,6 @@ require_once  __DIR__ . '/../src/vendor/autoload.php';
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-echo "<h2>catalogue service index</h2>";
-
 // Connexion à la DB
 $connection = new \MongoDB\Client("mongodb://dbcat");
 // Sélectionne la base de donnée à utiliser
@@ -14,13 +12,15 @@ $db_catalogue = $connection->catalogue;
 // Sélectionne la collection de sandwichs
 $sandwiches = $db_catalogue->sandwiches->find();
 
-foreach ($sandwiches as $sandwich) {
-    print $sandwich->nom . ' ' . $sandwich->type_pain . '<br>';
-}
+// foreach ($sandwiches as $sandwich) {
+//     print $sandwich->nom . ' ' . $sandwich->type_pain . '<br>';
+// }
 
 $app = new \Slim\App();
 
 $app->get("/sandwichs", function (Request $rq, Response $resp) {
+    $resp->getBody()->write("<h1>Hello world</h1>");
+    return $resp;
 });
 
 $app->run();
