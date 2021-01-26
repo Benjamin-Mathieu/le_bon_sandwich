a029fa3f159aa0d72d5833071f99c20af291c58a
Benjamin <ben88200@gmail.com>
2021-01-26 16:53:59 +0100

    Connexion bdd mongo

--- lbs_catalogue_service/api/index.php
+++ lbs_catalogue_service/api/index.php
@@ -1,8 +1,11 @@
 <?php
 
-#require_once  __DIR__ . '/../src/vendor/autoload.php';
+require_once  __DIR__ . '/../src/vendor/autoload.php';
 
-#use \Psr\Http\Message\ServerRequestInterface as Request ;
-#use \Psr\Http\Message\ResponseInterface as Response ;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
 echo "catalogue service index";
+
+$db_mongo = new \MongoDB\Client("mongodb://api.catalogue.local");
+echo "connected to mongo<br>";
