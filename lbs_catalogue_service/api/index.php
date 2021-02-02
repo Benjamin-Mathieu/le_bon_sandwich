4fbed57644568d494f526c27776223b4be039925
Benjamin <ben88200@gmail.com>
2021-02-02 16:35:28 +0100

    Ajout contrôleur getSandwichs Ajout du conteneur php-slim

--- lbs_catalogue_service/api/index.php
+++ lbs_catalogue_service/api/index.php
@@ -1,26 +1,22 @@
 <?php
 
-require_once  __DIR__ . '/../src/vendor/autoload.php';
-
+use lbs\catalogue\controller\CatalogueController;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-// Connexion à la DB
-$connection = new \MongoDB\Client("mongodb://dbcat");
-// Sélectionne la base de donnée à utiliser
-$db_catalogue = $connection->catalogue;
-// Sélectionne la collection de sandwichs
-$sandwiches = $db_catalogue->sandwiches->find();
+require_once  __DIR__ . '/../src/vendor/autoload.php';
 
-// foreach ($sandwiches as $sandwich) {
-//     print $sandwich->nom . ' ' . $sandwich->type_pain . '<br>';
-// }
+$config_slim = require_once('conf/Settings.php'); /* Récupération de la config de Slim */
+$errors = require_once('conf/Errors.php'); /* Récupération des erreurs */
 
-$app = new \Slim\App();
+/* Création du conteneur pour utiliser la cfg dans le programme */
+$container = new \Slim\Container(array_merge($config_slim, $errors));
 
-$app->get("/sandwichs", function (Request $rq, Response $resp) {
-    $resp->getBody()->write("<h1>Hello world</h1>");
-    return $resp;
+$app = new \Slim\App($container);
+
+$app->get("/sandwichs", function (Request $rq, Response $resp): Response {
+    $controller = new CatalogueController($this);
+    return $controller->getSandwichs($rq, $resp);
 });
 
 $app->run();
