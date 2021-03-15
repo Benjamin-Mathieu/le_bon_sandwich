40e4a3c58b8f2b00f218a85b60c03336f5bfbe07
GIRARDIN-VINCENT Bastien <bastien52300@hotmail.fr>
2021-03-15 11:53:29 +0100

    fusion code avec encore 1 erreur

--- lbs_catalogue_service/src/controller/CatalogueController.php
+++ lbs_catalogue_service/src/controller/CatalogueController.php
@@ -21,6 +21,8 @@ class CatalogueController
         // Sélectionne la base de donnée à utiliser
         $db_catalogue = $connection->catalogue;
 
+
+
         // ************* PAGINATION *************
         if ($rq->getQueryParams("page", "size")) {
 
@@ -33,21 +35,36 @@ class CatalogueController
             $count = $db_catalogue->sandwiches->count(); // Compte le nombre total de sandwichs de la collection sandwiches dans mongo
             $last_page = intdiv($count, $size) + 1;
 
+            // Récupération des sandwiches pour la pagination
+            if(isset($params['t'])){
+                $pain = $params["t"];
+                //var_dump($pain);
+                $sandwiches = $db_catalogue->sandwiches->find(
+                    ['type_pain' => $pain],
+                    [
+                        'limit' => 0 + $size,
+                        'skip' => ($current_page - 1) * $size,
+                        
+                        
+                    ]
+                );
+            }else{
+               // echo "gab est une sale pute";
+                $sandwiches = $db_catalogue->sandwiches->find(
+                    [],
+                    [
+                        'limit' => 0 + $size,
+                        'skip' => ($current_page - 1) * $size,
+                    ]
+                );
+            }
+
             // Condition si numéro de page supérieur à la dernière page alors retourner la dernière page
             if ($current_page > $last_page) $current_page = $last_page;
             // Condition si numéro de page inférieur à 1 alors retourne la première page
             if ($current_page < 1) $current_page = 1;
 
-
-            // Récupération des sandwiches pour la pagination
-            $sandwiches = $db_catalogue->sandwiches->find(
-                [],
-                [
-                    'limit' => 0 + $size,
-                    'skip' => ($current_page - 1) * $size
-                ]
-            );
-
+            //var_dump($sandwiches);
             // ********* JSON ***********
             $collection = array(
                 "type" => "collection",
@@ -78,18 +95,40 @@ class CatalogueController
                 array_push($collection['sandwichs'], $json_sandwich);
             }
 
-            $resp = $resp->withHeader('Content-Type', 'application/json');
+            $resp = $resp->withHeader('Content-Type', 'application/json', 'text/html');
             $resp->getBody()->write(json_encode($collection));
             return $resp;
+
+
+
         }
 
+
+
         // ************* PAR DEFAULT AFFICHAGE DES 10 PREMIERS SANDWICHS *************
-        $sandwiches = $db_catalogue->sandwiches->find(
-            [],
-            [
-                'limit' => 10
-            ]
-        );
+
+        if ($rq->getQueryParams()){
+            $params = $rq->getQueryParams();
+            if(isset($params['t'])){
+                $pain = $params["t"];
+                $sandwiches = $db_catalogue->sandwiches->find(
+                    ['type_pain' => $pain],
+                    [
+                        'limit' => 10,
+                    ]
+                );
+            }
+
+        }else{
+            $sandwiches = $db_catalogue->sandwiches->find(
+                [],
+                [
+                    'limit' => 10,
+                ]
+            );
+        }
+        
+
 
         $collection = array(
             "type" => "collection",
@@ -119,61 +158,6 @@ class CatalogueController
         $resp->getBody()->write(json_encode($collection));
         return $resp;
 
-    }
-
-    public function getSandwichsFilter(Request $rq, Response $resp, $args){
-
-        // Connexion à la DB
-        $connection = new \MongoDB\Client("mongodb://dbcat");
-        // Sélectionne la base de donnée à utiliser
-        $db_catalogue = $connection->catalogue;
-
-        if($rq->getQueryParams($default = null)){ // si le query params n'est pas null
-            $params = $rq->getQueryParams();
-            
-            if(isset($params['t'])){ // on va tester si le params t existe
-                $type_pain = $params['t'];
-                $type_array = array('type_pain' => $type_pain);
-                $sandwiches = $db_catalogue->sandwiches->find($type_array); // recherche des sandwichs selon le type de pain
-                
-                $collection = array(
-                    "type" => "collection",
-                    "count" => "",
-                    "date" => "",
-                    "sandwichs" => []
-                );
-                
-                $collection['date'] = date("Y/m/d");
-                $count = 0;
-
-                foreach ($sandwiches as $sandwich) {
-                    $s = array(
-                        "sandwich" => [
-                            "ref" => $sandwich->ref,
-                            "nom" => $sandwich->nom,
-                            "type_pain" => $sandwich->type_pain,
-                            "prix" => $sandwich->prix
-                        ]
-                    );
-                    $count++;
-                    array_push($collection['sandwichs'], $s);
-                }
-
-                $collection['count'] = $count;
-
-                if($count == 0){ // si count = 0 il n'y a donc pas de sandwich de ce type
-                    $collection['sandwichs'] = "Il n'y a pas de sandwichs de ce type";
-                    $resp = $resp->withHeader('Content-Type', 'application/json');
-                    $resp->getBody()->write(json_encode($collection));
-                    return $resp;
-
-                }else{
-                    $resp = $resp->withHeader('Content-Type', 'application/json');
-                    $resp->getBody()->write(json_encode($collection));
-                    return $resp;
-                }
-            }
-        }
     }
 
     public function getCategorie(Request $rq, Response $resp, $args){
--- lbs_commande_service/api/index.php
+++ lbs_commande_service/api/index.php
@@ -1,5 +1,4 @@
 <?php
-echo "commande service index";
 require_once  __DIR__ . '/../src/vendor/autoload.php';
 
 use \Psr\Http\Message\ServerRequestInterface as Request ;
