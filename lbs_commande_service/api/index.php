5b7d6e3802239945dc0691a44d8dda3670bb7150
Benjamin <ben88200@gmail.com>
2021-03-20 12:10:09 +0100

    Ajout middleware CORS

--- lbs_commande_service/api/index.php
+++ lbs_commande_service/api/index.php
@@ -1,6 +1,8 @@
 <?php
 require_once  __DIR__ . '/../src/vendor/autoload.php';
 
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 use lbs\command\controller\CommandeController;
 
 $config_slim = require_once('conf/Settings.php'); /* Récupération de la config de Slim */
@@ -17,10 +19,26 @@ $capsule->bootEloquent();
 
 $app = new \Slim\App($container);
 
+// Cors : voir doc (https://www.slimframework.com/docs/v3/cookbook/enable-cors.html)
+$app->add(\lbs\command\middlewares\Cors::class . ':checkCors');
+
+$app->options('/{routes:.+}', function (Request $rq, Response $rs) {
+    return $rs;
+});
+
 // *****************    ROUTES  *****************
 $app->post('/commandes', CommandeController::class . ':createCommand');
 
 $app->get('/commandes/{id}', CommandeController::class . ':getCommand')
     ->add(lbs\command\middlewares\Token::class . ":checkToken");
+
+$app->get("/test", CommandeController::class . ":test");
+
+// Catch-all route to serve a 404 Not Found page if none of the routes match
+// NOTE: make sure this route is defined last
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function ($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
+});
 
 $app->run();
