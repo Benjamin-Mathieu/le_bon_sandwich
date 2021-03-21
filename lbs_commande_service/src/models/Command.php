4c5546ffd35c35033645f3e3c671a5d653b279d6
Benjamin <ben88200@gmail.com>
2021-03-21 16:44:46 +0100

    Modification modèles

--- lbs_commande_service/src/models/Command.php
+++ lbs_commande_service/src/models/Command.php
@@ -7,8 +7,7 @@ class Command extends \Illuminate\Database\Eloquent\Model
     protected $table = "commande";
     protected $primareyKey = "id";
     protected $fillable = ["id", "nom", "mail", "livraison", "token"];
-    protected $hidden = ["created_at", "modified_at"];
-
+    protected $hidden = ["created_at", "updated_at"];
     public $incrementing = false;
     public $keyType = "string";
 }
