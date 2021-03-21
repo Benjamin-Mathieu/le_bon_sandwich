b3342c3c1c1525881b2b4e60bca4c69e51ffa984
Benjamin <ben88200@gmail.com>
2021-03-21 16:47:33 +0100

    Timestamps false

--- lbs_commande_service/src/models/Item.php
+++ lbs_commande_service/src/models/Item.php
@@ -7,7 +7,7 @@ class Item extends \Illuminate\Database\Eloquent\Model
     protected $table = "item";
     protected $primareyKey = "id";
     protected $fillable = ["id", "uri", "libelle", "tarif", "quantite", "command_id"];
-
+    public $timestamps = false;
     public $incrementing = false;
     public $keyType = "string";
 }
